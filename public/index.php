d71dcb3e9b237a2a5f19038fb7fda41c7578ce4d
Remixxlf <luisfilipeslima@gmail.com>
2026-09-04 23:56:28 -0300

    chore: add global exception catcher to index.php to debug 500 error

--- public/index.php
+++ public/index.php
@@ -11,6 +11,15 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 
 require __DIR__.'/../vendor/autoload.php';
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
+// Bootstrap Laravel and handle the request...
+try {
+    (require_once __DIR__.'/../bootstrap/app.php')
+        ->handleRequest(Illuminate\Http\Request::capture());
+} catch (\Throwable $e) {
+    http_response_code(500);
+    echo "<h1>CRITICAL ERROR TRACE</h1>";
+    echo "<b>Message:</b> " . $e->getMessage() . "<br><br>";
+    echo "<b>File:</b> " . $e->getFile() . ":" . $e->getLine() . "<br><br>";
+    echo "<b>Trace:</b><br><pre>" . $e->getTraceAsString() . "</pre>";
+    exit(1);
+}
